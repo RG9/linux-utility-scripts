3027d3383b515b5ecf5046129f59d84ab285d87e
Rafał Garbowski <rg9@rg9.pl>
2022-06-20 23:37:41 +0200

    Add stamp background to pdf

---
+++ add-stamp-background-to-pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## Example: ./add-stamp-background-to-pdf.sh doc.pdf test@example.com
+
+src_pdf=${1?provide path of pdf}
+target_pdf=$(echo "$src_pdf" | sed 's/.pdf/-stamped.pdf/')
+stamp_text=${2?provide stamp text}
+stamp_file="stamp-$RANDOM.pdf"
+
+./create-stamp-pdf.sh "$stamp_text" > "$stamp_file"
+
+pdftk "$src_pdf" background "$stamp_file" output "$target_pdf"
+
+rm "$stamp_file"
